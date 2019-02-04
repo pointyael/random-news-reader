c4ad7a4464d2d32107acc458c31ed0ad668e36c7
Elie Guedj <elie973@gmail.com>
2019-02-04 15:22:04 +0100

    About page created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'À propos' });
 });
 
 module.exports = router;
