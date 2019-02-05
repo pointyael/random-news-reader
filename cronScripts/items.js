3e02c7644f16edd6cb964b292bfd0b475814064b
Elie Guedj <elie973@gmail.com>
2019-02-05 19:15:48 +0100

    ébauches de models et cronScripts

---
+++ cronScripts/items.js
@@ -0,0 +1,15 @@
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+var retrieveItemsFromLink = function(link) {
+    let webFeed = await parser.parseURL(link);
+}
+
+var readItems = function(link) {
+    webfeed.items.forEach(item => {
+        console.log(item);
+    });
+}
+
+
+// TODO : Chopper les items de tous les feeds de la table source et les enregistrer dans la table item
---
+++ models/itemModel.js
@@ -0,0 +1,18 @@
+/*
+ite_id integer NOT NULL,
+    ite_name character varying(250),
+    ite_description character varying(400),
+    ite_type integer,
+    ite_link character varying(250),
+    ite_dateinsert date,
+    ite_language integer,
+    ite_theme integer,
+ite_source integer
+*/
+
+let name;
+let description;
+let type;
+let dateInsert;
+let language;
+let theme;
