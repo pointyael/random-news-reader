4e3c7fef896f270d4a45bf1067e426f4078865de
Elie Guedj <elie973@gmail.com>
2019-02-06 17:48:26 +0100

    Normalisation des items fonctionnelle

--- cronScripts/items.js
+++ cronScripts/items.js
@@ -1,19 +1,47 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
 
-var retrieveItemsFromLink = function(link) {
-    let webFeed = await parser.parseURL(link);
+async function retrieveItemsFromLink(link) {
+    let webFeed = parser.parseURL(link);
+    return webFeed;
 }
 
-var readItems = function(link) {
-    webfeed.items.forEach(item => {
-        console.log(item);
+async function getFeedInfo(feed){
+    let feedSchema = Object();
+    feedSchema = {
+        title: feed.title,
+        description: feed.description,
+        link: feed.link,
+        language: feed.language,
+        category: feed.category
+    }
+    return feedSchema;
+}
+
+async function feedToItemArray(feed){
+    let itemArray = [];
+    let itemSchema = Object();
+    feed.items.forEach(item => {
+        itemSchema = {
+            title: item.title,
+            description: item.description,
+            type: item.type,
+            datePub: item.datePub,
+            link: item.link,
+            language: item.language,
+            category: item.category
+          }
+          itemArray.push(itemSchema);
     });
+    return itemArray;
+}
+async function normalize(items) {
+
 }
 
 module.exports = {
     retrieveItemsFromLink,
-    readItems
+    feedToItemArray
 }
 
 
--- tests/itemsTest.js
+++ tests/itemsTest.js
@@ -1,1 +1,39 @@
 const items = require('../cronScripts/items');
+const expect = require('chai').expect;
+
+const link = "https://www.reddit.com/r/ProgrammerHumor.rss";
+
+var test = async function(link) {
+    var feed = await items.retrieveItemsFromLink(link);
+    var array = await items.feedToItemArray(feed);
+    return array;
+}
+
+test(link).then(console.log);
+
+/*
+describe("items Cron script", () => {
+    describe('"retrieveItemsFromLink"', () => {
+        before(function*() {
+            itemTable = items.retrieveItemsFromLink(link);
+
+        });
+        it('should be an object not empty', function * () {
+            expect(itemTable).to.be.an('object').not.empty
+        });
+        it('each items should have a title, link, and description attribute not empty', function * () {
+            itemTable.forEach(item => {
+                expect(item.title).to.be.a('string').not.empty;
+                expect(item.link).to.be.a('string').not.empty;
+                expect(item.description).to.be.a('string').not.empty;
+            });
+        });
+    })
+})
+*/
+
+/*items.retrieveItemsFromLink("https://www.reddit.com/r/ProgrammerHumor.rss").then(
+    items.readItems).catch(function(error){
+        console.log(error)
+    }
+);*/
