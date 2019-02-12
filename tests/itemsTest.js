d4a3dedbe2ff83ce8e21ee9894615fea6d48ad4c
Jason <>
2019-02-12 13:10:17 +0100

    test report generated

--- tests/itemsTest.js
+++ tests/itemsTest.js
@@ -32,14 +32,14 @@ describe("items Cron script", () => {
             expect(items).to.be.an('array').not.empty;
         });
 
-        // Marche pas 
+        // Marche pas
         /* AIDEZ MOI SVP */
         it.each(items, 'items should have a title and a link not empty', ['item'], function(item, next){
             expect(item.title).to.be.a('string').not.empty;
             expect(item.link).to.be.a('string').not.empty;
         });
 
-        
+
     });
 
     describe('"retrieveItemsFromLink"', () => {
