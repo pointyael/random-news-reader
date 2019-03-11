a2797a118c5a2ad583a4e40cc7ef952ec30e5825
Jason <jason.liebault@outlook.com>
2019-03-11 13:45:19 +0100

    Proc delete items older than 48 hours + test + function in the model

--- models/itemModel.js
+++ models/itemModel.js
@@ -2,15 +2,16 @@ var pgp = require("pg-promise")();
 var Items = require("../retrieveFromWeb/retrieveData.js");
 var FeedModel = require("./feedModel");
 
-const cn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
-};
+// const cn = {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS
+// };
 
-const db = pgp(cn);
+//const db = pgp(cn);
+var db = pgp("postgres://postgres:md5244af1e2823d5eaeeffc42c5096d8260@localhost:5432/randomizer");
 
 /*
 ite_id integer NOT NULL,
@@ -31,6 +32,22 @@ let dateInsert;
 let language;
 let theme;
 
+/* Query all items from db => only for test */
+const getAllItems = (request, response) =>
+{
+  db
+  .any("SELECT * from item")
+  .then
+  (
+    function(data)
+    {
+      response.status(200).json(data);
+    }
+  )
+  .catch(function (error)
+  { });
+}
+
 /* Query 12 random items from data base */
 const getRandomItems = (request, response) => {
     db.any("SELECT * FROM item ORDER BY RANDOM() LIMIT 12")
@@ -48,37 +65,52 @@ const insertItems = (request, response) => {
 
     db
     .any("SELECT * FROM source WHERE source.sou_id=2")
-    .then(function (data) {
-        data = data[0];
-        feedInfo = {
-            id: data.sou_id,
-            title: data.sou_title,
-            link: data.sou_link
-        }
-        feedInfoStringified = "'" + JSON.stringify(feedInfo).replace( /'/, "''") + "'::json";
-        Items
-        .getItems(data.sou_link)
-        .then
-        (
-            function(res){
-                itemsJsonString = (JSON.stringify(res));
-                //console.log(res);
-                db
-                .any("CALL \"insertNewItems\"("+feedInfoStringified+", '"+itemsJsonString+"')")
-                .then( () => response.status(200) )
-                .catch(function (error2) {
-                    console.log("ERROR3", error2);
-                });
-            }
-        )
-        .catch( function(err) { console.log("ERROR4 : ", err); })
+    .then(
+      function (data)
+      {
+          data = data[0];
+          feedInfo =
+          {
+              id: data.sou_id,
+              title: data.sou_title,
+              link: data.sou_link
+          }
 
-        })
-        .catch(function(err){ console.log("ERROR2:", err); });
+          feedInfoStringified = "'" + JSON.stringify(feedInfo).replace( /'/, "''") + "'::json";
+
+          Items
+          .getItems(data.sou_link)
+          .then
+          (
+              function(res){
+                  itemsJsonString = (JSON.stringify(res));
+                  //console.log(res);
+                  db
+                  .any("CALL \"insertNewItems\"("+feedInfoStringified+", '"+itemsJsonString+"')")
+                  .then( () => response.status(200) )
+                  .catch();
+              }
+          )
+          .catch()
+
+      }
+    )
+    .catch();
 
 }
 
+const deleteOldItems =
+() =>
+{
+  db
+  .any('CALL "deleteOldItemsProc"()')
+  .then()
+  .catch();
+}
+
 module.exports = {
+    getAllItems,
     getRandomItems,
-    insertItems
+    insertItems,
+    deleteOldItems
 }
