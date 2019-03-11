892678a03aa45642ba15116bc0f19c0d3ffebae4
Elie Guedj <elie973@gmail.com>
2019-03-11 16:33:16 +0100

    getFeedLinks from website url functionnal
    Former-commit-id: e8013b556a5fe79bf3be23adf52d502bf686db1e

---
+++ retrieveFromWeb/feedvalidator/src/feedvalidator/categories.py
@@ -0,0 +1,24 @@
+from .base import validatorBase
+from .category import category
+from .validators import yesno
+from .logging import ConflictingCatAttr, ConflictingCatChildren
+
+class categories(validatorBase):
+  def getExpectedAttrNames(self):
+    return [(None,u'scheme'),(None,u'fixed'),(None,u'href')]
+
+  def prevalidate(self):
+    self.validate_optional_attribute((None,'fixed'), yesno)
+
+    if (None,'href') in self.attrs:
+      if (None,'fixed') in self.attrs:
+        self.log(ConflictingCatAttr({'attr':'fixed'}))
+      if (None,'scheme') in self.attrs:
+        self.log(ConflictingCatAttr({'attr':'scheme'}))
+
+  def validate(self):
+    if (None,'href') in self.attrs and self.children:
+      self.log(ConflictingCatChildren({}))
+
+  def do_atom_category(self):
+    return category()
--- retrieveFromWeb/retrieveFeedLinks.js
+++ retrieveFromWeb/retrieveFeedLinks.js
@@ -1,4 +1,25 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
+// Use python shell
+let {PythonShell} = require('python-shell');
 
-// TODO : Utiliser un API pour chopper un maximum de feeds pour remplir la table source de la BDD
+function getWebsiteUrl(word) {
+
+}
+
+function getFeedLinks(url, callback){
+
+    var options = {
+        mode: 'text',
+        scriptPath: 'retrieveFromWeb/feedvalidator',
+        args: [url]
+    };
+
+    PythonShell.run('feedfinder.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        callback(results);
+    });
+}
+
+module.exports = {
+    getFeedLinks
+}
