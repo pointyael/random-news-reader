fc53b4e34a176116748fe72e2234b7ad10d1a122
JasLieb <jason.liebault@outlook.com>
2019-04-04 10:47:05 +0200

    build repaired on local

--- retrieveFromWeb/retrieveFeedLinks.js
+++ retrieveFromWeb/retrieveFeedLinks.js
@@ -2,19 +2,21 @@
 let {PythonShell} = require('python-shell');
 
 function getFeedLinks(url, callback){
-  console.log(process.platform);
+  //console.log(process.env);
     var options = {
-        pythonPath: process.platform != "win32" ? "python3" : "py", //'/bin/python2',
+        pythonPath: process.platform != "win32" ? "python" : 'C:\\Python27\\python.exe',
+                                               // "python3" : "py", if python 3 compatibility ok
         mode: 'text',
         scriptPath: 'retrieveFromWeb/feedvalidator',
         args: [url]
     };
 
     PythonShell.run('feedfinder.py', options, function (err, results) {
-        if (err) { console.log(err); throw err; }
+        if (err) throw err;
+        if (results[0].length == 0 ) results = new Array();
         callback(results);
     });
-    console.log(PythonShell.getVersionSync());
+
 }
 
 module.exports = {
