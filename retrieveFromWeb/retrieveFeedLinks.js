b60a6008998953e5af0456922fbe67b0fc854f09
JasLieb <jas.lieb@hotmail.com>
2019-04-07 13:02:11 +0200

    add python win (default) and linux for travisCI path into .env

--- retrieveFromWeb/retrieveFeedLinks.js
+++ retrieveFromWeb/retrieveFeedLinks.js
@@ -4,7 +4,7 @@ let {PythonShell} = require('python-shell');
 function getFeedLinks(url, callback){
 
     var options = {
-        pythonPath: '/bin/python2',
+        pythonPath: process.platform != "win32" ? process.env.PYPATHLIX : process.env.PYPATHWIN,//'/bin/python2',
         mode: 'text',
         scriptPath: 'retrieveFromWeb/feedvalidator',
         args: [url]
