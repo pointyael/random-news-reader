cb36411ed1ba8e7baff823cf011dcf087c13e6bd
JasLieb <jason.liebault@outlook.com>
2019-04-11 13:54:26 +0200

    set connectionString as normal parameter, not as json object parameter

--- database/dbFactory.js
+++ database/dbFactory.js
@@ -3,6 +3,6 @@ const pgp = require("pg-promise")();
 
 const connectionString = process.env.DB_USER + '://' + process.env.DB_USER + ':' +  process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME ;
 
-const db = pgp({ connectionString });
+const db = pgp(connectionString);
 
 module.exports = { db };
