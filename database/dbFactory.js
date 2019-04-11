b4adec8b1e651afb8618e1f324f9a03af2bd4bd2
JasLieb <jason.liebault@outlook.com>
2019-04-11 14:00:44 +0200

    add require dotenv config

--- database/dbFactory.js
+++ database/dbFactory.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const pgp = require("pg-promise")();
+require('dotenv').config();
 
 const connectionString = process.env.DB_USER + '://' + process.env.DB_USER + ':' +  process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME ;
 
